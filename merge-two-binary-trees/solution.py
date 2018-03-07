0dd2537fd97611e4731cca0bc01b9c7eef947c15
lsl <songlin.lin@yundata.com>
2018-03-07 20:37:45 +0800

    增加merge-two-binary-trees的解法

--- merge-two-binary-trees/solution.py
+++ merge-two-binary-trees/solution.py
@@ -7,26 +7,28 @@ class Solution(object):
         """
 
         def merge(t1, t2, n):
-
+            print(id(t1), id(t2), id(n))
             if t1 is None and t2 is None:
                 return
 
             if t1 is not None and t2 is None:
-                n.val = t1.val
                 return
 
             if t1 is None and t2 is not None:
-                n.val = t2.val
                 return
 
             n.val = t1.val + t2.val
-            n.left = t1.left or t2.left
             if n.left:
                 merge(t1.left, t2.left, n.left)
 
-            n.right = t1.right or t2.right
+            n.left = t1.left or t2.left
+
             if n.right:
+                if id(t1.right) == id(t2.right):
+                    raise ValueError()
                 merge(t1.right, t2.right, n.right)
+
+            n.right = t1.right or t2.right
 
         root = t1 or t2
         if root:
--- merge-two-binary-trees/test_solution.py
+++ merge-two-binary-trees/test_solution.py
@@ -4,7 +4,15 @@ from solution import Solution
 
 def test_solution():
     s = Solution()
-    tree1 = TreeNode(1)
-    tree2 = TreeNode(1)
-    print(s.mergeTrees(tree1, tree2))
-    assert False
+    tree1 = TreeNode(1,
+            TreeNode(3, TreeNode(5)),
+            TreeNode(2))
+    tree2 = TreeNode(2,
+            TreeNode(1,
+                None,
+                TreeNode(4)),
+            TreeNode(3,
+                None,
+                TreeNode(7)))
+    tree3 = s.mergeTrees(tree1, tree2)
+    print(tree3)

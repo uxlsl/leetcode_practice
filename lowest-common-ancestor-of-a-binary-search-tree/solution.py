104ef8afeba3f99816f1d63857481425e7f8afbc
lsl <songlin.lin@yundata.com>
2018-03-08 21:24:40 +0800

    增加lowest-common-ancestor-of-a-binary-search-tree的解法

--- lowest-common-ancestor-of-a-binary-search-tree/solution.py
+++ lowest-common-ancestor-of-a-binary-search-tree/solution.py
@@ -1,18 +1,4 @@
 class Solution(object):
-    def find(self, node, n, path):
-        if node is None:
-            return False
-        if node == n:
-            return True
-        for i in [node.left, node.right]:
-            if i:
-                path.append(n)
-                if self.find(i, n, path):
-                    return True
-                else:
-                    path.pop()
-        return False
-
     def lowestCommonAncestor(self, root, p, q):
         """
         :type root: TreeNode
@@ -20,22 +6,17 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
-        if root is None or p is None or q is None:
+        if root is None:
             return None
-
-        l1 = []
-        self.find(root, p, l1)
-        if q in l1:
-            return q
-
-        l2 = []
-        self.find(root, q, l2)
-        if p in l2:
-            return q
-
-        for i in range(min(len(l1), len(l2))):
-            if l1[i] != l2[i]:
-                return l1[i-1]
-        else:
-            return l1[i]
-
+        if p.val < root.val and q.val < root.val:
+            x = self.lowestCommonAncestor(root.left,p,q)
+            if x:
+                return x
+        if p.val > root.val and q.val > root.val:
+            y = self.lowestCommonAncestor(root.right,p,q)
+            if y:
+                return y
+        if p.val <= root.val <= q.val:
+            return root
+        if q.val <= root.val <= p.val:
+            return root
--- lowest-common-ancestor-of-a-binary-search-tree/test_solution.py
+++ lowest-common-ancestor-of-a-binary-search-tree/test_solution.py
@@ -4,6 +4,21 @@ from solution import Solution
 
 def test_solution():
     s = Solution()
-    n = TreeNode(2)
+    n = TreeNode(1)
     t = TreeNode(1, n)
     assert s.lowestCommonAncestor(t, t, n) == t
+
+    p = TreeNode(0)
+    q = TreeNode(5)
+    tree = TreeNode(6,
+            TreeNode(2,
+                p,
+                TreeNode(4,
+                    TreeNode(3),
+                    q
+                    )),
+            TreeNode(8,
+                TreeNode(7),
+                TreeNode(9)))
+    assert s.lowestCommonAncestor(tree, p, q).val == 2
+

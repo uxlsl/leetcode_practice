4353cccf03bb5e1bef8fbbc2ecf67e994c8b2024
lsl <songlin.lin@yundata.com>
2018-08-18 15:12:06 +0800

    增加翻转二叉树的解法

---
+++ invert-binary-tree/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def invertTree(self, root):
+        if root:
+            root.left, root.right = root.right, root.left
+            self.invertTree(root.left)
+            self.invertTree(root.right)
+        return root

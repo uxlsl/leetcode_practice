a2b98f465085274ec26e7c9ad345cbce7287606b
lsl <songlin.lin@yundata.com>
2018-08-18 17:09:33 +0800

    优化k-dirr-pair的算法2

--- k-diff-pairs-in-an-array/solution.py
+++ k-diff-pairs-in-an-array/solution.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findPairs(self, nums, k):
+    def findPairs1(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
@@ -10,24 +10,41 @@ class Solution:
         from collections import defaultdict
         m = defaultdict(int)
         for i in nums:
-                m[i] += 1
+            m[i] += 1
 
         result = set()
         for i in nums:
 
             m[i] -= 1
-            m[i-k] -= 1
-            if m[i] >= 0 and m[i-k] >= 0:
-                result.add((i-k, i))
+            m[i - k] -= 1
+            if m[i] >= 0 and m[i - k] >= 0:
+                result.add((i - k, i))
             m[i] += 1
-            m[i-k] += 1
+            m[i - k] += 1
 
             m[i] -= 1
-            m[i+k] -= 1
-            if m[i] >= 0 and m[i+k] >= 0:
-                result.add((i, i+k))
+            m[i + k] -= 1
+            if m[i] >= 0 and m[i + k] >= 0:
+                result.add((i, i + k))
             m[i] += 1
-            m[i+k] += 1
- 
+            m[i + k] += 1
+
         return len(result)
 
+    def findPairs(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if k < 0:
+            return 0
+        m = {}
+        for i, v in enumerate(nums):
+            m[v] = i
+        count = 0
+        for j, v in enumerate(nums):
+            if v + k in m and m[v + k] != j:
+                count += 1
+                del m[v + k]  # 为什么
+        return count

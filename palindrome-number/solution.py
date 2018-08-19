41d357205a6b4821ab65ea84baf211d641db0a7a
lsl <songlin.lin@yundata.com>
2018-08-19 08:45:59 +0800

    增加回文数算法

---
+++ palindrome-number/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        s = str(x)
+        i, j = 0, len(s) - 1
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True

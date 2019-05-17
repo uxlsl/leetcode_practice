48b41984006dfe68e87c6f3ffdb83bf52a5666ff
lin <songlin.lin@yunfangdata.com>
2019-05-17 18:44:16 +0800

    临时增加一个解题方法

--- sliding-window-maximum/solution.py
+++ sliding-window-maximum/solution.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def maxSlidingWindow(self, nums, k):
+    def maxSlidingWindow1(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
@@ -13,4 +13,23 @@ class Solution(object):
             stack.pop(0)
             stack.append(i)
             result.append(max(stack))
+        return result
+
+    def maxSlidingWindow(self, nums, k):
+        if k == 0:
+            return []
+        result = []
+        stack = [(0,nums[0])]
+        for index,value in enumerate(nums[1:]):
+            if stack[-1][1] < value:
+                while stack:
+                    if stack[-1][1] < value:
+                        stack.pop()
+                    else:
+                        stack.append((index, value))
+                        break
+                if len(stack) == 0:
+                    stack.append((index, value))
+            print(stack)
+            result.append(stack[0][1])
         return result

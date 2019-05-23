978eac9c7b9cf3a9eaea344c05d99db2d66b6374
lin <songlin.lin@yunfangdata.com>
2019-05-23 17:10:37 +0800

    增加零钱兑换很慢的算法

--- coin-change/solution.py
+++ coin-change/solution.py
@@ -5,16 +5,22 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        if amount < 0:
-            return -1
-        elif amount == 0:
-            return 0
-        m = -1
-        for coin in coins:
-            count = self.coinChange(coins, amount-coin)
-            if count >= 0:
-                if m > 0:
-                    m = min(count + 1, m)
-                else:
-                    m = count + 1
-        return m
+        cache = {}
+        def f(coins,amount):
+            if amount < 0:
+                return -1
+            elif amount == 0:
+                return 0
+            if amount in cache:
+                return cache[amount]
+            m = -1
+            for coin in coins:
+                count = f(coins, amount-coin)
+                if count >= 0:
+                    if m > 0:
+                        m = min(count + 1, m)
+                    else:
+                        m = count + 1
+            cache[amount] = m
+            return m
+        return f(coins, amount)

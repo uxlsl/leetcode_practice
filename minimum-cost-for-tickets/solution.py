19b248d08981422804ed6a243f8b442e8b889a98
lin <songlin.lin@yunfangdata.com>
2019-05-24 17:46:06 +0800

    增加官方解法

--- minimum-cost-for-tickets/solution.py
+++ minimum-cost-for-tickets/solution.py
@@ -9,6 +9,7 @@ class Solution(object):
         m = {0:1,1:7,2:30}
         while days:
             best_choice = 0
+            avg_price = costs[0]
             eat = {0:1}
             for choice in [1,2]:
                 start = days[0]
@@ -22,9 +23,25 @@ class Solution(object):
                         break
                     count += 1
                 else:
-                    eat[choice] = index
-                if costs[choice] / count < costs[0]:
+                    eat[choice] = index + 1
+                if costs[choice] / count < avg_price:
                     best_choice = choice
+                    avg_price = costs[choice] / count
             total += costs[best_choice]
             days = days[eat[best_choice]:]
         return total
+    def mincostTickets(self, days, costs):
+        dayset = set(days)
+        durations = [1, 7, 30]
+
+        @lru_cache(None)
+        def dp(i):
+            if i > 365:
+                return 0
+            elif i in dayset:
+                return min(dp(i + d) + c
+                           for c, d in zip(costs, durations))
+            else:
+                return dp(i + 1)
+
+        return dp(1)

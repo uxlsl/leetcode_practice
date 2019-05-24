fac39166433c7b2fc37018e8b70a5e92d1fc013b
lin <songlin.lin@yunfangdata.com>
2019-05-24 16:12:03 +0800

    增加错误的最低票价算法

--- minimum-cost-for-tickets/solution.py
+++ minimum-cost-for-tickets/solution.py
@@ -5,3 +5,26 @@ class Solution(object):
         :type costs: List[int]
         :rtype: int
         """
+        total = 0
+        m = {0:1,1:7,2:30}
+        while days:
+            best_choice = 0
+            eat = {0:1}
+            for choice in [1,2]:
+                start = days[0]
+                end = start + m[choice] - 1
+                count = 0
+                avg_price = costs[0]
+                index = None
+                for index,day in enumerate(days):
+                    if day > end:
+                        eat[choice] = index
+                        break
+                    count += 1
+                else:
+                    eat[choice] = index
+                if costs[choice] / count < costs[0]:
+                    best_choice = choice
+            total += costs[best_choice]
+            days = days[eat[best_choice]:]
+        return total

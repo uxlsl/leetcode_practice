a362b377a739996f445135c08c1c770e5e651f7c
lsl <songlin.lin@yundata.com>
2019-05-27 21:59:50 +0800

    增加城市调度的错误算法

--- two-city-scheduling/solution.py
+++ two-city-scheduling/solution.py
@@ -4,3 +4,18 @@ class Solution(object):
         :type costs: List[List[int]]
         :rtype: int
         """
+        costs = sorted(costs, key=lambda x: x[0])
+        result = 0
+        while costs:
+            a = costs[0][0]
+            b = costs[1][1]
+            min_cost = costs[0][0] + costs[1][1]
+            min_index = 1
+            for index, item in enumerate(costs[1:], 1):
+                if min_cost > a + item[1]:
+                    min_cost = a + item[1]
+                    min_index = index
+            del costs[min_index]
+            del costs[0]
+            result += min_cost
+        return result

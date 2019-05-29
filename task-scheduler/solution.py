bbc5091067d014b304b9d9b3b3715faf31c371c6
lin <songlin.lin@yunfangdata.com>
2019-05-29 11:53:37 +0800

    修改错误的调度算法

--- task-scheduler/solution.py
+++ task-scheduler/solution.py
@@ -10,17 +10,21 @@ class Solution(object):
             if i not in m:
                 m[i] = [0,0]
             m[i][1] += 1
-
         length = 0
-        while m:
-            for k,v in m.items():
-                if length >= v[0]:
-                    v[1] -= 1
+        keys = [i[0] for i in
+                sorted(m.items(), key=lambda x:x[1][1], reverse=True)]
+        results = []
+        while keys:
+            for k in list(keys):
+                if m[k][1] > 0 and length >= m[k][0]:
+                    m[k][1] -= 1
+                    if m[k][1] == 0:
+                        keys.remove(k)
                     length += 1
-                    v[0] = length + n
-                    if v[1] == 0:
-                        del m[k]
+                    m[k][0] = length + n
+                    results.append(k)
                     break
             else:
+                results.append('-')
                 length += 1
-        return length
+        return results

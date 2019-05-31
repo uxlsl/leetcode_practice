5a24025456c63e60fbfcf389305e55a3f66e7700
lin <songlin.lin@yunfangdata.com>
2019-05-31 14:35:52 +0800

    增加按奇偶排序数组 II的解法

---
+++ sort-array-by-parity-ii/solution.py
@@ -0,0 +1,24 @@
+# leetcode 922
+# https://leetcode-cn.com/submissions/detail/19812328/
+
+class Solution(object):
+    def sortArrayByParityII(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        odd = []
+        even = []
+        for i in A:
+            if i % 2 == 0:
+                even.append(i)
+            else:
+                odd.append(i)
+
+        result = []
+        for i,j in zip(odd,even):
+            result.append(i)
+            result.append(j)
+
+        return result
+

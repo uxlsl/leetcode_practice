17c5a2594e9603ee4c083e86e4b3e8fe1f51eda5
lsl <songlin.lin@yundata.com>
2019-06-02 08:20:46 +0800

    增加一个错误的解法

---
+++ range-sum-of-bst/solution.py
@@ -0,0 +1,23 @@
+tion for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rangeSumBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        else:
+            total =  self.rangeSumBST(root.left, L, R) + self.rangeSumBST(root.right, L, R)
+            if L < root.val < R:
+                total += root.val
+            return total
+

9e4dfb8c074fd0f8aa45c488aa251eee7fef686c
lin <songlin.lin@yunfangdata.com>
2019-06-04 10:56:58 +0800

    增加高度检查正确的解法

--- height-checker/solution.py
+++ height-checker/solution.py
@@ -1,13 +1,6 @@
 # leetcode
 # https://leetcode-cn.com/problems/height-checker/
 
-def lcs(X, Y, m, n):
-    if m == 0 or n == 0:
-       return 0;
-    elif X[m-1] == Y[n-1]:
-       return 1 + lcs(X, Y, m-1, n-1);
-    else:
-       return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
 
 class Solution(object):
     def heightChecker(self, heights):
@@ -15,5 +8,9 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        heights_1 = sorted(heights)
-        return len(heights) - lcs(heights, heights_1 , len(heights), len(heights_1))
+        res = 0
+        for x,y in zip(heights, sorted(heights)):
+            if x != y:
+                res += 1
+        return res
+

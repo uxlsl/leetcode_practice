81e09e11749a85d1fd9c65cd5f4885eeae59da38
lin <songlin.lin@yunfangdata.com>
2019-06-06 11:38:35 +0800

    增加一些说明

---
+++ word-break-ii/solution.py
@@ -0,0 +1,10 @@
+# leetcode 140
+# https://leetcode-cn.com/problems/word-break-ii/
+
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: List[str]
+        """

f3a2522cd2b2896d9f423db4cf30f0b47b54e47e
lin <songlin.lin@yunfangdata.com>
2019-06-06 14:55:06 +0800

    单词拆分慢的解法

--- word-break-ii/solution.py
+++ word-break-ii/solution.py
@@ -1,6 +1,3 @@
-# leetcode 140
-# https://leetcode-cn.com/problems/word-break-ii/
-
 class Solution(object):
     def wordBreak(self, s, wordDict):
         """
@@ -8,3 +5,22 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: List[str]
         """
+        def f(s, path, word_set, results):
+            if s == '':
+                results.append(list(path))
+                return
+            word = ''
+            i = 0
+            while i < len(s):
+                word +=s[i]
+                if word in word_set:
+                    path.append(word)
+                    f(s[i+1:])
+                    path.pop()
+                i += 1
+
+        results = []
+        word_set = set(wordDict)
+        path = []
+        f(s,path, word_set, results)
+        return [' '.join(i) for i in results]

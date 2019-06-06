64775dbd72fb28ce2a7290f05551dcadfca24468
lin <songlin.lin@yunfangdata.com>
2019-06-06 17:52:58 +0800

    修改两数之和 II - 输入有序数组的解法

--- two-sum-ii-input-array-is-sorted/solution.py
+++ two-sum-ii-input-array-is-sorted/solution.py
@@ -6,7 +6,12 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        for i in range(0, len(numbers)):
-            for j in range(i+1, len(numbers)):
-                if numbers[i] + numbers[j] == target:
-                    return [i+1,j+1]
+        i,j = 0, len(numbers) - 1
+
+        while i < j:
+            if numbers[i] + numbers[j] > target:
+                j -= 1
+            elif numbers[i] + numbers[j] < target:
+                i += 1
+            else:
+                return [i+1, j+1]

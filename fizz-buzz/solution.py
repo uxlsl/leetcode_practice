bdb7229768c0e445de2df50782a883815998b934
lsl <songlin.lin@yundata.com>
2019-06-07 21:59:07 +0800

    修改Fizz Buzz的解法说明

--- fizz-buzz/solution.py
+++ fizz-buzz/solution.py
@@ -15,6 +15,7 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        # 也可以以乘代除
         result = []
         for i in range(1,n+1):
             if i % 3 == 0 and i % 5 == 0:

a1e144573fc2b1fd2677c1cce75e674cbe4bb67e
lsl <songlin.lin@yundata.com>
2019-06-07 13:53:56 +0800

    增加sqrtx的解法

---
+++ sqrtx/solution.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        def square(guess):
+            return guess * guess
+
+        def goodenough(guess, x):
+            if abs(square(guess) - x) < 0.1:
+                return True
+            else:
+                return False
+
+        def improve(guess, x):
+            return (x/guess + guess) / 2
+
+        def sqrtIter(guess, x):
+            if goodenough(guess, x):
+                return guess
+            else:
+                return sqrtIter(improve(guess, x), x)
+
+        return int(sqrtIter(1, x))

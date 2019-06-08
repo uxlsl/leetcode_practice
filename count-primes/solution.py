fbe6e3381a566b396dea0cf838610763ac83911f
lsl <songlin.lin@yundata.com>
2019-06-08 08:38:41 +0800

    修改计算质数数量算法

--- count-primes/solution.py
+++ count-primes/solution.py
@@ -1,3 +1,5 @@
+# https://coolshell.cn/articles/3738.html
+
 class Solution(object):
     def countPrimes(self, n):
         """
@@ -20,3 +22,21 @@ class Solution(object):
                     b.append(j)
             a = b
             i += 1
+
+
+
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        l = [i for i in range(n)]
+        for j in range(2, n):
+            if l[j] != 0:
+                k = 2
+                while k * j < n:
+                    l[k * j] = 0
+                    k += 1
+        count = 0
+        for n in l:
+            if n !=0 and n!=1:
+                count += 1
+        return count

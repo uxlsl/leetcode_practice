657885f199037f2b40c0ade417ed7e7ef8ec6956
lin <songlin.lin@yunfangdata.com>
2019-06-10 18:36:48 +0800

    增加买卖股票的最佳时机 II的解法参考

--- best-time-to-buy-and-sell-stock-ii/solution.py
+++ best-time-to-buy-and-sell-stock-ii/solution.py
@@ -28,3 +28,19 @@ class Solution(object):
             ret += prices[i-1] - prices[x]
 
         return ret
+
+class Solution1(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) <=1:
+            return 0
+
+        total = 0
+        for i in range(1,len(prices)):
+            if prices[i]>prices[i-1]:
+                total += prices[i]-prices[i-1]
+
+        return total

2cd6d67d56a99c4143c74f608fc163dd8b247344
lin <songlin.lin@yunfangdata.com>
2019-06-12 14:36:22 +0800

    增加字符的最短距离的解法

---
+++ shortest-distance-to-a-character/solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def shortestToChar(self, S, C):
+        """
+        :type S: str
+        :type C: str
+        :rtype: List[int]
+        """
+        ret = [float('inf')]*len(S)
+
+        for index,c in enumerate(S):
+            if c == C:
+                i = index
+                while i >= 0 and ret[i] != 0:
+                    ret[i]=min(index-i,ret[i])
+                    i -= 1
+
+                i = index+1
+                while i < len(S) and ret[i] != 0:
+                    ret[i]=min(i-index,ret[i])
+                    i += 1
+
+        return ret
---
+++ shortest-distance-to-a-character/test_solution.py
@@ -0,0 +1,6 @@
+from solution import Solution
+
+
+def test_solution():
+    s = Solution()
+    assert s.shortestToChar(S = "loveleetcode", C = 'e') == [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]

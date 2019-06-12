3fc9c21e48cfad3db94139a9b52c22d8b0b13ea7
lin <songlin.lin@yunfangdata.com>
2019-06-12 15:22:16 +0800

    修改三角形的最大周长的解法

--- largest-perimeter-triangle/solution.py
+++ largest-perimeter-triangle/solution.py
@@ -12,3 +12,32 @@ class Solution(object):
 
         return ret
 
+    def largestPerimeter(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        ret = 0
+        A = sorted(A, reverse=True)
+        for i in range(0,len(A)):
+            a = A[i]
+            for j in range(i+1, len(A)):
+                b = A[j]
+                find = False
+                for k in range(j+1,len(A)):
+                    c = A[k]
+                    if a < b + c and b < a + c and c < a + b:
+                        ret = max(ret,a+b+c)
+                        find = True
+                        break
+                if find:
+                    break
+        return ret
+
+class Solution(object):
+    def largestPerimeter(self, A):
+        A.sort()
+        for i in xrange(len(A) - 3, -1, -1):
+            if A[i] + A[i+1] > A[i+2]:
+                return A[i] + A[i+1] + A[i+2]
+        return 0

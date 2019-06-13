8b4090bf4d85eeaa3679e26e82dbc61046fa15bb
lin <songlin.lin@yunfangdata.com>
2019-06-13 16:01:38 +0800

    二叉树中第二小的节点的解法

--- second-minimum-node-in-a-binary-tree/solution.py
+++ second-minimum-node-in-a-binary-tree/solution.py
@@ -1,8 +1,21 @@
+import heapq
+
 class Solution(object):
     def findSecondMinimumValue(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        if root is None:
+
+        def f(node, vals):
+            if node is None:
+                return
+            vals.append(node.val)
+            f(node.left,vals)
+            f(node.right,vals)
+        vals = []
+        f(root, vals)
+        if vals < 2:
             return -1
+        vals = heapq.nsmallest(2, vals)
+        return vals[1]

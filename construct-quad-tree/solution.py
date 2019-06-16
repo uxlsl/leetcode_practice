503483eaddf56d3ab1ca934c7050526d9000f98a
lsl <songlin.lin@yundata.com>
2019-06-16 08:21:05 +0800

    修改四叉树的解法

--- construct-quad-tree/solution.py
+++ construct-quad-tree/solution.py
@@ -26,18 +26,26 @@ class Solution(object):
                         return False
 
             return True
+        def print_grid(grid, x1,y1,x2,y2):
+            print('#'*10)
+            print(x1,y1,x2,y2)
+            for x in range(x1, x2+1):
+                for y in range(y1,y2+1):
+                    print(grid[x][y], end=' ')
+                print()
+            print('#'*10)
 
         def f(grid,x1,y1,x2,y2):
             if is_same(grid,x1,y1,x2,y2):
-                return Node(grid[x1][y1],True,None,None,None,None)
+                return Node(grid[x1][y1] == 1,True,None,None,None,None)
             else:
                 x_mid = (x1+x2)//2
                 y_mid = (y1+y2)//2
                 # 对边界有点不确定
                 return Node(-1, False,
                         f(grid, x1,y1, x_mid,y_mid),
-                        f(grid,x_mid+1,y1, x2, y_mid),
                         f(grid,x1,y_mid+1, x_mid, y2),
+                        f(grid,x_mid+1,y1, x2, y_mid),
                         f(grid,x_mid+1,y_mid+1, x2, y2),
                 )
 

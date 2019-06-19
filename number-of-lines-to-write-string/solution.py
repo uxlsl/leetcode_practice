a3d7647da582e891408eab3b010ae30cc6b8b635
lin <songlin.lin@yunfangdata.com>
2019-06-19 14:47:50 +0800

    增加写字符串需要的行数的正确的解法

--- number-of-lines-to-write-string/solution.py
+++ number-of-lines-to-write-string/solution.py
@@ -6,9 +6,11 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        total = sum(widths[ord(c) - ord('a')] for c in S)
-        if total % 100 == 0:
-            return [total // 100, 0]
-        else:
-            return [total // 100 + 1, total % 100]
-
+        total = 0
+        for c in S:
+            total_n =  total + widths[ord(c) - ord('a')]
+            if total_n % 100 != 0 and total//100 != total_n//100:
+                total = total + 100 - total % 100 + widths[ord(c) - ord('a')]
+            else:
+                total = total_n
+        return  [total // 100 + 1, total % 100]

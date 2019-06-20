b6441cefd9d899523e925274b9fd3d2ba6384469
lin <songlin.lin@yunfangdata.com>
2019-06-20 18:13:30 +0800

    增加二叉树的堂兄弟节点的解法

--- cousins-in-binary-tree/solution.py
+++ cousins-in-binary-tree/solution.py
@@ -1,15 +1,14 @@
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution(object):
     def isCousins(self, root, x, y):
-        """
-        :type root: TreeNode
-        :type x: int
-        :type y: int
-        :rtype: bool
-        """
+        parent = {}
+        depth = {}
+        def dfs(node, par = None):
+            if node:
+                depth[node.val] = 1 + depth[par.val] if par else 0
+                parent[node.val] = par
+                dfs(node.left, node)
+                dfs(node.right, node)
+
+        dfs(root)
+        return depth[x] == depth[y] and parent[x] != parent[y]
 

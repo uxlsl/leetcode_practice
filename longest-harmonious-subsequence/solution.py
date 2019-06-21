00f42da4d8820e8c5bfeeca0ec757cae18ef1542
lin <songlin.lin@yunfangdata.com>
2019-06-21 16:42:38 +0800

    修改最长和谐子序列的解法

--- longest-harmonious-subsequence/solution.py
+++ longest-harmonious-subsequence/solution.py
@@ -7,17 +7,18 @@ class Solution(object):
         :rtype: int
         """
         if len(nums) <= 1:
-            return len(nums)
+            return 0
         a = Counter(nums).most_common()
+        if len(a) == 1:
+            return 0
         a = sorted(a, key=lambda x:x[0])
         l = None
         for i in range(0, len(a)-1):
             if abs(a[i][0] - a[i+1][0])<=1:
                 if l is None:
-                    l = a[i][1] - a[i+1][1]
+                    l = a[i][1] + a[i+1][1]
                 else:
-                    l = max(l, a[i][1] - a[i+1][1])
-
-        return l
-
-
+                    l = max(l, a[i][1] + a[i+1][1])
+        if l:
+            return l
+        return 0

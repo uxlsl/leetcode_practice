f6ffa795a2ef902a07b1f7e2d931e2c153a7d3e2
lin <songlin.lin@yunfangdata.com>
2019-06-26 15:08:26 +0800

    修改回旋镖的数量的解法

--- number-of-boomerangs/solution.py
+++ number-of-boomerangs/solution.py
@@ -2,25 +2,21 @@
 # https://leetcode-cn.com/problems/number-of-boomerangs/submissions/
 # 解法:通过等价距离,然后再排列
 
+class Solution:
+    def distance(self, p1, p2):
+        # 注意 a**2 + b**2 比 a*a + b*b 耗时！
+        return (p1[0]-p2[0])*(p1[0]-p2[0]) + (p1[1]-p2[1])*(p1[1]-p2[1])
 
-class Solution(object):
-    def numberOfBoomerangs(self, points):
-        """
-        :type points: List[List[int]]
-        :rtype: int
-        """
-        from collections import defaultdict
-        m = defaultdict(lambda :defaultdict(list))
-
-        for i in range(len(points)):
-            for j in range(i+1, len(points)):
-                dist = (points[i][0] - points[j][0])**2 + (points[i][1] - points[j][1])**2
-                m[dist][i].append(j)
-                m[dist][j].append(i)
-        ret = 0
-        for v in m.values():
-            for vv in v.values():
-                if len(vv) > 1:
-                    ret += len(vv)*(len(vv)-1)
-
-        return ret
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        if not points:
+            return 0
+        res = 0
+        for point in points:
+            dic = dict()
+            for tmp in points:
+                if point != tmp:
+                    dist = self.distance(point, tmp)
+                    if dist not in dic:
+                        dic[dist] = 1
+                    else:
+                        dic[dist] += 1

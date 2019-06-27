40619d1a88fe6b704465100254de17c0728d3c34
lin <songlin.lin@yunfangdata.com>
2019-06-27 15:15:07 +0800

    修改

--- reach-a-number/solution.py
+++ reach-a-number/solution.py
@@ -11,4 +11,21 @@ class Solution(object):
                 return f(to+n, target,n+1)
             else:
                 return f(to-n, target, n+1)
-        return f(0,target, 1)
+        if target < 0:
+            target = -target
+        import math
+        n = int(math.sqrt(2*target)) - 3
+        return f(n*(n+1)/2,target, n+1)
+
+class Solution(object):
+    def reachNumber(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        point, step = 0, 0
+        target = abs(target)
+        while target > point or (point >= target and (point - target)%2 != 0):
+            step += 1
+            point += step
+        return step

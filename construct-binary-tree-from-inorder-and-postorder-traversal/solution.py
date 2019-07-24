1bc96246696daaf45d8415e0a360591e1bfb5adb
lin <linsl2018@163.com>
2019-07-24 15:37:16 +0800

    增加从中序与后序遍历序列构造二叉树的错误解法

---
+++ construct-binary-tree-from-inorder-and-postorder-traversal/solution.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        def f(inorder, postorder, start, end):
+            if postorder and start <= end:
+                v = postorder.pop()
+                node = TreeNode(v)
+                i = inorder.index(v)
+                node.left = f(inorder, postorder, start, i - 1)
+                node.right = f(inorder, postorder, i + 1, end)
+                return node
+            return None
+        return f(inorder, postorder, 0, len(inorder) - 1)

ad7e844e07d23ff3206d8bb1338f77d11dbffd43
lin <linsl2018@163.com>
2019-07-31 10:34:50 +0800

    增加所有可能的满二叉树的解法

--- all-possible-full-binary-trees/solution.py
+++ all-possible-full-binary-trees/solution.py
@@ -1,9 +1,20 @@
+# leetcode
+# https://leetcode-cn.com/problems/all-possible-full-binary-trees/
+# 递归解决
+# 类似格雷码
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+
+import copy
+import functools
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
 
 class Solution(object):
     def allPossibleFBT(self, N):
@@ -11,3 +22,21 @@ class Solution(object):
         :type N: int
         :rtype: List[TreeNode]
         """
+        @functools.lru_cache(None)
+        def f(N):
+            root = TreeNode(0)
+            if N == 1:
+                return [root]
+            if N % 2 == 0:
+                return []
+            ret = []
+            for i in range(1, N, 2):
+                left = f(i)
+                right = f(N-1-i)
+                for l in left:
+                    for r in right:
+                        root.left = l
+                        root.right = r
+                        ret.append(copy.deepcopy(root))
+            return ret
+        return f(N)

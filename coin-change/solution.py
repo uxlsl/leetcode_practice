a9ec3a7ef6b32d16e839a457515db06d5589d4f3
lin <linsl2018@163.com>
2019-08-07 16:16:43 +0800

    增加322. 零钱兑换的官方解法

--- coin-change/solution.py
+++ coin-change/solution.py
@@ -1,24 +1,46 @@
 # leetcode
 # https://leetcode-cn.com/problems/coin-change/
 # 尽量选择最大的
+import functools
 
 
 class Solution(object):
-    def coinChange(self, coins, amount):
+
+    def coinChange1(self, coins, amount):
         """
         :type coins: List[int]
         :type amount: int
         :rtype: int
         """
-        def f(coins, amount):
-            if amount == 0:
-                return True
-            elif amount < 0:
-                return False
-            else:
-                for i in coins[::-1]:
-                    if f(coins, amount - i):
-                        return True
-                return False
+        result = -1
 
-        return f(coins, amount)
+        @functools.lru_cache(None)
+        def f(coins, amount,count):
+            nonlocal result
+            if amount == 0:
+                if result == -1:
+                    result = count
+                result = min(count,result)
+                return
+            elif amount < 0:
+                return
+            else:
+                for i in coins:
+                    f(coins, amount - i,count+1)
+
+        coins = sorted(coins, reverse=True)
+        f(tuple(coins), amount, 0)
+        return result
+
+    def coinChange(self, coins, amount):
+        max = amount + 1
+        dp = [max]*(amount+1)
+        dp[0] = 0
+
+        for i in range(1,amount+1):
+            for j in range(len(coins)):
+                if coins[j] <= i:
+                    dp[i] = min(dp[i], dp[i-coins[j]]+1)
+        if dp[amount] <= amount:
+            return dp[amount]
+        return -1

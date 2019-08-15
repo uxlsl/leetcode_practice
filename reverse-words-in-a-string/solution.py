3bde9daa2949e8d5c2bd523b36d2e65ec38b30bf
lin <linsl2018@163.com>
2019-08-15 16:20:49 +0800

    增加151. 翻转字符串里的单词的解法

---
+++ reverse-words-in-a-string/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        words = s.split()
+        return ' '.join(reversed(words))

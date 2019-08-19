219e06dad6eddc02cb43b477f62b8fdfb0887937
lin <linsl2018@163.com>
2019-08-19 11:14:26 +0800

    增加最长等差数列的错误的解法

--- longest-arithmetic-sequence/solution.py
+++ longest-arithmetic-sequence/solution.py
@@ -1,18 +1,28 @@
+import pprint
 class Solution(object):
     def longestArithSeqLength(self, A):
         """
         :type A: List[int]
         :rtype: int
         """
-        ret = 0
+        if len(A) <=1:
+            ret = 0
+            return
+        ret = 2
         m = {}
         for i in range(len(A)):
             for j in range(i):
                 diff = A[i] - A[j]
-                if diff in m and m[diff][-1] == j:
-                    m[diff].append(i)
+                if diff not in m:
+                    m[diff] = []
+                # bug ，如果连接等差一样，但不同
+                for k in m[diff]:
+                    if k[-1] == j:
+                        k.append(i)
+                        if len(k) > ret:
+                            print(k)
+                            ret = len(k)
+                        break
                 else:
-                    m[diff] = [j, i]
-                ret = max(len(m[diff]), ret)
-        print(m)
+                    m[diff].append([j, i])
         return ret

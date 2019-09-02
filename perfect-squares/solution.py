42cbc406fc25504cf96cdb4170cf225ff041d1e5
lin <linsl2018@163.com>
2019-09-02 11:24:52 +0800

    增加完全平方数通用解法

--- perfect-squares/solution.py
+++ perfect-squares/solution.py
@@ -20,3 +20,14 @@ class Solution(object):
 
         mem = {}
         return f(n, mem)
+
+    def numSquares(self, n):
+        dp = [i for i in range(n+1)]
+
+        for i in range(1,n+1):
+            j = 1
+            while i - j*j >=0:
+                dp[i] = min(dp[i], dp[i-j*j]+1)
+                j += 1
+
+        return dp[n]

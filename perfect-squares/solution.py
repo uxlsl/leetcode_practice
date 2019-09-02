9eff7a701698f7a7d933951c78a431ccf2495d3f
lin <linsl2018@163.com>
2019-09-02 11:06:37 +0800

    增加完全平方数超时解法

--- perfect-squares/solution.py
+++ perfect-squares/solution.py
@@ -11,8 +11,10 @@ class Solution(object):
         def f(n, mem):
             if n == 0:
                 return 0
+            if n in mem:
+                return mem[n]
             x = int(math.sqrt(n))
-            count = min([f(n - i * i, mem) + 1 for i in range(1, x + 1)])
+            count = min([f(n - i * i, mem) + 1 for i in reversed(range(1, x + 1))])
             mem[n] = count
             return count
 

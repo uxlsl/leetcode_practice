ccfc625f3477df1dd8a0d00e3f4d71807d362289
lin <linsl2018@163.com>
2019-09-24 15:35:33 +0800

    增加移掉K位数字的解法

--- remove-k-digits/solution.py
+++ remove-k-digits/solution.py
@@ -6,21 +6,24 @@ class Solution(object):
         :rtype: str
         """
         num = list(num)
+        i = 0
+        while k > 0 and i < len(num)-1:
+            if int(num[i]) > int(num[i + 1]):
+                del num[i]
+                k -= 1
+                if i > 0:
+                    i -= 1
+            else:
+                i += 1
 
         for _ in range(k):
-            if len(num) == 1:
-                return '0'
-            for i in range(len(num)-1):
-                if int(num[i]) > int(num[i+1]):
-                    del num[i]
-                    break
-            else:
-                del num[-1]
-
+            if len(num) == 0:
+                return "0"
+            del num[-1]
         j = 0
-        while j < len(num) and num[j] == '0':
+        while j < len(num) and num[j] == "0":
             j += 1
         num = num[j:]
         if len(num) == 0:
-            return '0'
-        return ''.join(num)
+            return "0"
+        return "".join(num)

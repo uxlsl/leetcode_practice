6355eae0c8d2c39e798a5dfe606f56c53b0955ee
lin <linsl2018@163.com>
2019-09-25 14:58:19 +0800

    修改738. 单调递增的数字的解法

--- monotone-increasing-digits/solution.py
+++ monotone-increasing-digits/solution.py
@@ -10,17 +10,13 @@ class Solution(object):
         :type N: int
         :rtype: int
         """
-        n = str(N)
-        num = []
-        for i in n:
-            num.append(i)
-
-        nine = len(num)
-        for i in range(len(num) - 1, 0, -1):
-            if num[i - 1] > num[i]:
-                num[i - 1] = str(int(num[i - 1]) - 1)
+        N = list(str(N))
+        nine = len(N)
+        for i in range(len(N) - 1, 0, -1):
+            if N[i - 1] > N[i]:
+                N[i - 1] = str(int(N[i - 1]) - 1)
                 nine = i
 
-        num = ''.join(num)
-        num = int(num[:nine] + '9' * (len(num) - nine))
-        return num
+        N = ''.join(N)
+        N = int(N[:nine] + '9' * (len(N) - nine))
+        return N

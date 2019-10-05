143b1a6af8861f8fe0608277e2b4363dc75acf48
lsl <songlin.lin@yundata.com>
2019-10-05 08:44:15 +0800

    增加字符串的排列的解法3

--- permutation-in-string/solution.py
+++ permutation-in-string/solution.py
@@ -1,6 +1,3 @@
-from collections import Counter
-
-
 class Solution(object):
     def checkInclusion(self, s1, s2):
         """
@@ -8,14 +5,25 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
+        def f(s):
+            ret = [0] * 26
+            for c in s:
+                ret[index(c)] += 1
+            return ret
+
+        def index(c):
+            return ord(c) - ord('a')
+
         if len(s1) > len(s2):
             return False
+        s2 += 'a'
+        x = f(s1)
+        y = f(s2[:len(s1)])
 
-        x = Counter(s1)
-
-        for i in range(len(s2)-len(s1)+1):
-            y = Counter(s2[i:i+len(s1)])
+        for i in range(1,len(s2)-len(s1)+1):
             if x == y:
                 return True
+            y[index(s2[i-1])] -= 1
+            y[index(s2[i+len(s1)-1])] += 1
 
         return False

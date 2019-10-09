30f6689dca277c28a924e81c5e6acecad2212702
lin <linsl2018@163.com>
2019-10-09 16:00:19 +0800

    增加1184. 公交站间的距离的解法

---
+++ distance-between-bus-stops/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def distanceBetweenBusStops(self, distance, start, destination):
+        """
+        :type distance: List[int]
+        :type start: int
+        :type destination: int
+        :rtype: int
+        """
+        if start > destination:
+            start, destination = destination, start
+
+        a = sum(distance[start:destination])
+        b = sum(distance) - a
+        return min(a,b)

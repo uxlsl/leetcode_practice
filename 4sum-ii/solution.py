fb074bdba7f88619df714d9282fbce919faced80
lin <linsl2018@163.com>
2019-10-17 15:36:12 +0800

    增加454. 四数相加 II的不超时的解法

--- 4sum-ii/solution.py
+++ 4sum-ii/solution.py
@@ -25,3 +25,7 @@ class Solution(object):
                         tmp[j+k] += v
             m = tmp
         return m.get(0, 0)
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        import collections
+        dic = collections.Counter(a + b for a in A for b in B)
+        return sum(dic.get(- c - d, 0) for c in C for d in D)

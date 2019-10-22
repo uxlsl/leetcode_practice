dbed42a4c00fe83b5faf49e72283d8d940589bd5
lin <linsl2018@163.com>
2019-10-22 15:14:45 +0800

    增加240. 搜索二维矩阵 II的解法

---
+++ search-a-2d-matrix-ii/solution.py
@@ -0,0 +1,13 @@
+ass Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if target == matrix[i][j]:
+                    return True
+
+        return False

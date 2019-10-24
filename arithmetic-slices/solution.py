49418fbd4ca1e82bf19cc13816aefb283018a383
lin <linsl2018@163.com>
2019-10-24 17:51:05 +0800

    增加413. 等差数列划分的超时解法

--- arithmetic-slices/solution.py
+++ arithmetic-slices/solution.py
@@ -13,13 +13,22 @@ class Solution(object):
         :type A: List[int]
         :rtype: int
         """
+        m = {}
 
         def isArithmetic(A, i, j):
-            d = A[i + 1] - A[i]
-            for x in range(i + 1, j):
-                if A[x] != A[x + 1] - d:
-                    return False
-            return True
+            if (i,j-1) in A:
+                if A[(i,j-1)] and A[j-1] - A[j-2] == A[j] - A[j-1]:
+                    m[(i,j)] = True
+                else:
+                    m[(i,j)] = False
+            else:
+                d = A[i + 1] - A[i]
+                for x in range(i + 1, j):
+                    if A[x] != A[x + 1] - d:
+                        m[(i,j)] = False
+                        return False
+                m[(i,j)] = True
+                return True
 
         # 看划分
 
@@ -28,5 +37,4 @@ class Solution(object):
             for j in range(i +2, len(A)):
                 if isArithmetic(A,i,j):
                     count += 1
-
         return count

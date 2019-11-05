17f28f4182419f77fab467a538a14c0874d45bec
lin <linsl2018@163.com>
2019-11-05 15:10:54 +0800

    增加973. 最接近原点的 K 个点的解法

---
+++ k-closest-points-to-origin/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        return sorted(points, key=lambda x:x[0]**2 + x[1]**2)[:K]

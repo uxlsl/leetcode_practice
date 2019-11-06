3985378c4048027cb2d77166707039bf2b291517
lin <linsl2018@163.com>
2019-11-06 10:34:45 +0800

    优化221. 最大正方形的解法

--- maximal-square/solution.py
+++ maximal-square/solution.py
@@ -13,33 +13,38 @@ class Solution(object):
         if len(matrix) == 0:
             return 0
         m = {}
+        max_l = 0
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == "1":
-                    m[i, j, 0] = True
+                    max_l = 1
+                    m[i, j, 1] = True
                 else:
-                    m[i, j, 0] = False
-        max_l = 0
+                    m[i, j, 1] = False
         L = min(len(matrix), len(matrix[0]))
-        for l in range(1, L + 1):
+        for l in range(2, L + 1):
+            can = False
             for i in range(len(matrix)):
                 for j in range(len(matrix[0])):
-                    if i + l >= len(matrix) or j + l >= len(matrix[0]):
+                    if i + l - 1 >= len(matrix) or j + l - 1 >= len(matrix[0]):
                         continue
                     if m[i, j, l - 1]:
                         # 检查增加的边是否全为'1'
                         for z in range(l):
                             if (
-                                matrix[i + z][j + l] == "0"
-                                or matrix[i + l][j + z] == "0"
+                                matrix[i + z][j + l - 1] == "0"
+                                or matrix[i + l - 1][j + z] == "0"
                             ):
                                 m[i, j, l] = False
                                 break
                         else:
+                            can = True
                             m[i, j, l] = True
                             max_l = max(max_l, l)
                     else:
                         m[i, j, l] = False
 
-        max_l += 1
+            if not can:
+                break
+
         return max_l * max_l

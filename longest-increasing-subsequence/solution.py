d589f0841f5be10c4e70a571d9c014c560dda004
lin <linsl2018@163.com>
2019-11-07 11:08:07 +0800

    增加300. 最长上升子序列的解法2

--- longest-increasing-subsequence/solution.py
+++ longest-increasing-subsequence/solution.py
@@ -12,7 +12,7 @@ class Solution(object):
             count = 1
             cur = nums[i]
             pre = None
-            for j in range(i+1, len(nums)):
+            for j in range(i + 1, len(nums)):
                 if cur < nums[j]:
                     count += 1
                     pre = cur
@@ -25,3 +25,25 @@ class Solution(object):
             max_length = max(max_length, count)
 
         return max_length
+
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+
+        dp = {}
+        dp[0] = 1
+        ret = 1
+        for i in range(1, len(nums)):
+            val = 0
+            for j in range(0, i):
+                if nums[j] < nums[i]:
+                    val = max(val, dp[j])
+
+            dp[i] = val + 1
+            ret = max(dp[i], ret)
+
+        return ret

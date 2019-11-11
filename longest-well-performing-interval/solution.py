dfcd0681ca41747891cca8a1598f7c639c2e1d2b
lin <linsl2018@163.com>
2019-11-11 11:17:34 +0800

    增加1124. 表现良好的最长时间段错误的解法的超时算法

--- longest-well-performing-interval/solution.py
+++ longest-well-performing-interval/solution.py
@@ -26,3 +26,28 @@ class Solution(object):
             far = max(far, l)
 
         return far
+
+    def longestWPI(self, hours):
+        """
+        :type hours: List[int]
+        :rtype: int
+        """
+
+        # 超时算法
+
+        totals = [0]
+
+        for i in hours:
+            if i > 8:
+                a = 1
+            else:
+                a = -1
+            totals.append(totals[-1] + a)
+
+        l = 0
+        for i in range(len(hours)):
+            for j in range(0, i+1):
+                if totals[i + 1] - totals[j] > 0:
+                    l = max(l, i - j + 1)
+                    break
+        return l

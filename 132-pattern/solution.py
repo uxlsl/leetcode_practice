33e6ebfb1452524d3f9b0546f717a79c06fa3294
lin <linsl2018@163.com>
2019-11-14 11:04:56 +0800

    增加132模式正确解法

--- 132-pattern/solution.py
+++ 132-pattern/solution.py
@@ -1,26 +1,30 @@
+# leetcode
+# https://leetcode-cn.com/problems/132-pattern/solution/132mo-shi-by-leetcode-2/
+
+
 class Solution(object):
-    def find132pattern(self, nums):
+    def find132pattern1(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        for i in range(len(nums)-2):
-            for j in range(i+1,len(nums)-1):
+        for i in range(len(nums) - 2):
+            for j in range(i + 1, len(nums) - 1):
                 if num[i] >= nums[j]:
                     continue
-                for k in range(j+1, len(nums)):
+                for k in range(j + 1, len(nums)):
                     if nums[i] < nums[k] < nums[j]:
                         return True
         return False
 
-    def find132pattern(self, nums):
+    def find132pattern2(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-        for i in range(len(nums)-2):
+        for i in range(len(nums) - 2):
             l = [nums[i]]
-            for j in range(i+1,len(nums)):
+            for j in range(i + 1, len(nums)):
                 if len(l) == 1:
                     if l[0] < nums[j]:
                         l.append(nums[j])
@@ -29,5 +33,30 @@ class Solution(object):
                         l[1] = nums[j]
                     elif l[0] < nums[j] < l[1]:
                         return True
+
+        return False
+
+    def find132pattern(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) < 3:
+            return False
+
+        stack = []
+        X = {}
+        X[0] = nums[0]
+
+        for i in range(1, len(nums)):
+            X[i] = min(X[i - 1], nums[i])
+
+        for j in range(len(nums) - 1, -1, -1):
+            if nums[j] > X[j]:
+                while stack and stack[-1] <= X[j]:
+                    stack.pop()
+                if stack and stack[-1] < nums[j]:
+                    return True
+                stack.append(nums[j])
 
         return False

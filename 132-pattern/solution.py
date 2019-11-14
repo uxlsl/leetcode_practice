1d62169af9dceb5ed3f3e67ac45a321ba0d916d0
lin <linsl2018@163.com>
2019-11-14 10:30:49 +0800

    增加132模式2超时解法

--- 132-pattern/solution.py
+++ 132-pattern/solution.py
@@ -12,3 +12,22 @@ class Solution(object):
                     if nums[i] < nums[k] < nums[j]:
                         return True
         return False
+
+    def find132pattern(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        for i in range(len(nums)-2):
+            l = [nums[i]]
+            for j in range(i+1,len(nums)):
+                if len(l) == 1:
+                    if l[0] < nums[j]:
+                        l.append(nums[j])
+                else:
+                    if l[1] < nums[j]:
+                        l[1] = nums[j]
+                    elif l[0] < nums[j] < l[1]:
+                        return True
+
+        return False

265a22a80741b6f9bccf2907f7ded43883dded05
lin <linsl2018@163.com>
2019-11-25 17:39:32 +0800

    增加1109. 航班预订统计超时解法

---
+++ corporate-flight-bookings/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def corpFlightBookings(self, bookings, n):
+        """
+        :type bookings: List[List[int]]
+        :type n: int
+        :rtype: List[int]
+        """
+        # 超时了
+        arr = [0]*n
+
+        for book in bookings:
+            for i in range(book[0], book[1]+1):
+                arr[i-1]+=book[2]
+
+        return arr

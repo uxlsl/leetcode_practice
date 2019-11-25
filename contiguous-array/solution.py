9ee9d9d1c5a5c581b533eb6f377ec581ea5d3808
lin <linsl2018@163.com>
2019-11-25 17:08:59 +0800

    增加525. 连续数组的题目

---
+++ contiguous-array/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findMaxLength(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        # 0 0 0 1 1 1
+        # 0 1
+        # 0 1 0

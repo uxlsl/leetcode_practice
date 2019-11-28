4838e012de8baa928970c2f0836f482033f92952
lin <linsl2018@163.com>
2019-11-28 14:59:34 +0800

    增加826. 安排工作以达到最大收益的解法

--- most-profit-assigning-work/solution.py
+++ most-profit-assigning-work/solution.py
@@ -1,8 +1,65 @@
+# leetcode
+# 一个工人都最多只能安排一个工作，但是一个工作可以完成多次。
+# 优化问题!
+
+
 class Solution(object):
-    def leastInterval(self, tasks, n):
+    def maxProfitAssignment1(self, difficulty, profit, worker):
         """
-        :type tasks: List[str]
-        :type n: int
+        :type difficulty: List[int]
+        :type profit: List[int]
+        :type worker: List[int]
+        :rtype: int
+        """
+        total = 0
+        m = {}
+        for w in worker:
+            x = 0
+            if w in m:
+                x = m[w]
+            else:
+                for d, p in zip(difficulty, profit):
+                    if w >= d:
+                        x = max(x, p)
+                m[w] = x
+            total += x
+
+        return total
+
+    def maxProfitAssignment(self, difficulty, profit, worker):
+        """
+        :type difficulty: List[int]
+        :type profit: List[int]
+        :type worker: List[int]
         :rtype: int
         """
 
+        dp = [[i, j] for i, j in zip(difficulty, profit)]
+        dp = sorted(dp, key=lambda x: x[0])
+
+        v = dp[0][1]
+
+        # 排能力和最大收益
+
+        for i in dp[1:]:
+            if v < i[1]:
+                v = i[1]
+            else:
+                i[1] = v
+
+        worker = sorted(worker)
+        total = 0
+        cur = 0
+
+        for w in worker:
+            if w < dp[cur][0]:
+                continue
+            while cur < len(dp) - 1:
+                if w < dp[cur + 1][0]:
+                    break
+                cur += 1
+            if w > dp[-1][0]:
+                cur = len(dp) - 1
+            total += dp[cur][1]
+
+        return total

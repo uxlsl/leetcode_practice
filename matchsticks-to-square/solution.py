d0381b506b829a9e451c2eb08981be3ffb24ff84
lin <linsl2018@163.com>
2019-11-28 11:30:01 +0800

    修改473. 火柴拼正方形的解法

--- matchsticks-to-square/solution.py
+++ matchsticks-to-square/solution.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def makesquare(self, nums):
+    def makesquare1(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
@@ -31,3 +31,36 @@ class Solution(object):
             return False
         avg = total / 4
         return f(len(nums)-1, (avg, avg, avg, avg))
+
+    def canPartitionKSubsets(self, nums, k):
+        target, rem = divmod(sum(nums), k)
+        if rem: return False
+
+        def search(groups):
+            if not nums: return True
+            v = nums.pop()
+            for i, group in enumerate(groups):
+                if group + v <= target:
+                    groups[i] += v
+                    if search(groups): return True
+                    groups[i] -= v
+                if not group: break
+            nums.append(v)
+            return False
+
+        nums.sort()
+        if nums[-1] > target: return False
+        while nums and nums[-1] == target:
+            nums.pop()
+            k -= 1
+
+        return search([0] * k)
+
+    def makesquare(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if not nums:
+            return False
+        return self.canPartitionKSubsets(nums, 4)

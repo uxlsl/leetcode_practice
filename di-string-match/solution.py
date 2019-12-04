e79f46db36780a0db1cd32c1e75b0c46b9ef48e2
lin <linsl2018@163.com>
2019-12-04 11:34:41 +0800

    增加942. 增减字符串匹配的解法

--- di-string-match/solution.py
+++ di-string-match/solution.py
@@ -1,21 +1,24 @@
 # leetcode 942
 # https://leetcode-cn.com/problems/di-string-match/
+# 给定只含 "I"（增大）或 "D"（减小）的字符串 S ，令 N = S.length。
+
+# 返回 [0, 1, ..., N] 的任意排列 A 使得对于所有 i = 0, ..., N-1，都有：
+
+# 如果 S[i] == "I"，那么 A[i] < A[i+1]
+# 如果 S[i] == "D"，那么 A[i] > A[i+1]
+# https://leetcode-cn.com/problems/di-string-match/solution/zeng-jian-zi-fu-chuan-pi-pei-by-leetcode/
 
 class Solution(object):
     def diStringMatch(self, S):
-        """
-        :type S: str
-        :rtype: List[int]
-        """
-        if len(S) < 1:
-            return []
+        lo, hi = 0, len(S)
+        ans = []
+        for x in S:
+            if x == 'I':
+                ans.append(lo)
+                lo += 1
+            else:
+                ans.append(hi)
+                hi -= 1
 
-        result = [0]
-        i = 0
-        for c in S:
-            if c == 'I':
-                result.append(result[i]+1)
-            elif c == 'D':
-                result.append(result[i]-1)
+        return ans + [lo]
 
-        return result
